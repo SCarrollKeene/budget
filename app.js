f353fca9a3b3b5fc4e03ba824bc81f407bc5c84c
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-06-14 09:13:57 -0600

    added two event listeners to the global app controller and created a ctrlAddItem function that will be referenced multiple times later

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // IIFE
+// Budget Controller
 let budgetController = (function() {
-    let x = 23;
-    let add = function(a) {
-        return x + a;
-    }
 
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-})();
-
-let UIController = (function() {
     // some code
+
 })();
 
+// UI Controller
+let UIController = (function() {
+
+    // some code
+
+})();
+
+// Global App Controller
 let controller = (function(budgetCtrl, UICtrl) {
-    let z = budgetCtrl.publicTest(5);
-    return {
-        anPublic: function() {
-            console.log(z);
-        }
+
+    let ctrlAddItem = function() {
+        // 1. Get field input data
+
+        // 2. Add item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        console.log("it works.");
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // Global Keypress event listener
+    document.addEventListener('keypress', function(e) {
+
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
 })(budgetController, UIController);
+
