e850129661ff1131e433ea00ff071472795ee7ea
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-08-01 09:14:00 -0600

    added private DOMstrings method to UI Controller & exposed method for public use, also added public getInput function inside UI Controller that returns values from user input. getInput is called inside of ctrlAddItem func in global app controller

--- app.js
+++ app.js
@@ -8,16 +8,39 @@ let budgetController = (function() {
 
 // UI Controller
 let UIController = (function() {
+    // private method
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
 
-    // some code
+    // public method that IIFE will return
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }; 
+        },
+
+        getDOMstrings: function() {
+            // expose private DOMstrings method to public
+            return DOMstrings;
+        }
+    };
 
 })();
+
 
 // Global App Controller
 let controller = (function(budgetCtrl, UICtrl) {
 
     let ctrlAddItem = function() {
         // 1. Get field input data
+        let input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -26,9 +49,12 @@ let controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        console.log("it works.");
+        //console.log("it works.");
     }
 
+    // a standalone function can simply be passed into an event listener
+    // doesn't need paranthesis to be a callback, event listener calls it
+    // for us
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     // Global Keypress event listener
