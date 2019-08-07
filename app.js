107e781bd2b345c03e2cb8b903e5ebb1d87e7f13
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-08-06 19:32:53 -0600

    added private array data structure and public addItem function into budgetController. Created newItem = budgetCtrl.addItem(); inside of global controller

--- app.js
+++ app.js
@@ -14,6 +14,47 @@ let budgetController = (function() {
         this.value = value;
     };
 
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            let newItem;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push into our data structure
+            data.allItems[type].push(newItem);
+
+            // return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
+
 })();
 
 // UI Controller
@@ -65,10 +106,13 @@ let controller = (function(budgetCtrl, UICtrl) {
     };
 
     let ctrlAddItem = function() {
+        let input, newItem;
+
         // 1. Get field input data
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add item to the UI
 
