4a33580df0a57ce35846d14cc85aef4f6e936039
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-08-06 18:51:25 -0600

    created init function that was exposed and made public in order for app to function and put event listeners into setupEventListeners func that is used inside of the init func

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ let UIController = (function() {
     let DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add__value'
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     }
 
     // public method that IIFE will return
@@ -37,10 +38,25 @@ let UIController = (function() {
 // Global App Controller
 let controller = (function(budgetCtrl, UICtrl) {
 
+    let setupEventListeners = function() {
+        let DOM = UICtrl.getDOMstrings();
+        // a standalone function can simply be passed into an event listener
+        // doesn't need paranthesis to be a callback, event listener calls it
+        // for us
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        // Global Keypress event listener
+        document.addEventListener('keypress', function(e) {
+
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     let ctrlAddItem = function() {
         // 1. Get field input data
         let input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -50,21 +66,15 @@ let controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
         //console.log("it works.");
-    }
+    };
 
-    // a standalone function can simply be passed into an event listener
-    // doesn't need paranthesis to be a callback, event listener calls it
-    // for us
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    // Global Keypress event listener
-    document.addEventListener('keypress', function(e) {
-
-        if (e.keyCode === 13 || e.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('App has started.');
+            setupEventListeners();
         }
-
-    });
+    }
 
 })(budgetController, UIController);
 
+controller.init();
