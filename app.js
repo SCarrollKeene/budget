d1eabfe89b17496c3949fe960760ece9ea53b59b
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-08-14 09:33:27 -0600

    added private calculateTotal method in budget controller, added budget and percentage to data structure, created public calculateBudget and getBudget methods in budget controller, called calculateBudget & getBudget methods inside of the newly created updateBudget function within the global app controller. Also called update budget inside of ctrlAddItem at step 5

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ let budgetController = (function() {
         this.value = value;
     };
 
+    let calculateTotal = function(type) {
+        let sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     let data = {
         allItems: {
             exp: [],
@@ -22,7 +30,9 @@ let budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1 // -1 means something is non-existent
     };
 
     return {
@@ -48,6 +58,34 @@ let budgetController = (function() {
 
             // return the new element
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            // calculate total income & expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // calculate budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+                // fixes "infinity" percentage if exp is added before inc in ui
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
         },
 
         testing: function() {
@@ -153,10 +191,13 @@ let controller = (function(budgetCtrl, UICtrl) {
     let updateBudget = function() {
 
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. return the budget
+        let budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
+        console.log(budget);
 
     };
 
